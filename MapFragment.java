f50593cec7686a6d03ecb4b02f8e3a9662320110
Risa-j <132641080+Risa-j@users.noreply.github.com>
2023-05-07 20:15:58 +0800

    Add files via upload

---
+++ MapFragment.java
@@ -0,0 +1,227 @@
+package com.example.clf318.ui.me.map;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.baidu.location.BDAbstractLocationListener;
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.search.core.PoiInfo;
+import com.baidu.mapapi.search.core.SearchResult;
+import com.baidu.mapapi.search.poi.OnGetPoiSearchResultListener;
+import com.baidu.mapapi.search.poi.PoiDetailResult;
+import com.baidu.mapapi.search.poi.PoiDetailSearchResult;
+import com.baidu.mapapi.search.poi.PoiIndoorResult;
+import com.baidu.mapapi.search.poi.PoiNearbySearchOption;
+import com.baidu.mapapi.search.poi.PoiResult;
+import com.baidu.mapapi.search.poi.PoiSearch;
+import com.example.clf318.R;
+import com.example.clf318.base.BaseFragment2;
+
+
+
+public class MapFragment extends Fragment {
+
+    private static final int REQUEST_CODE =0x1001 ;
+    private MapView mMapView;
+    private BaiduMap mBaiduMap;
+    private LocationClient mLocationClient;
+    private PoiSearch mPoiSearch;
+    //private LatLng latLng;//经纬度
+
+    public  class MyPoiOverlay extends PoiOverlay{
+
+        /**
+         * 构造函数
+         *
+         * @param baiduMap 该 PoiOverlay 引用的 BaiduMap 对象
+         */
+        public MyPoiOverlay(BaiduMap baiduMap) {
+            super(baiduMap);
+        }
+
+        @Override
+        public boolean onPoiClick(int i) {
+            if (getPoiResult().getAllPoi() != null) {
+                PoiInfo poiInfo = getPoiResult().getAllPoi().get(i);
+                if (poiInfo != null) {
+                    Toast.makeText(getContext(),
+                                    "名称："+poiInfo.name
+                                    +"\n地址"+poiInfo.address
+                                    +"\n电话"+poiInfo.phoneNum, Toast.LENGTH_LONG)
+                            .show();
+                }
+            }
+            return super.onPoiClick(i);
+        }
+    }
+
+
+    //构造地图数据
+    public class MyLocationListener extends BDAbstractLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            //mapView 销毁后不在处理新接收的位置
+            if (location == null || mMapView == null){
+                return;
+            }
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    //.direction(location.getDirection()).latitude(location.getLatitude())
+                    //.longitude(location.getLongitude()).build();
+            .latitude(31.270157)
+                    .longitude(120.745253).build();
+            //latLng=new LatLng(location.getLatitude(),location.getLongitude());
+            mBaiduMap.setMyLocationData(locData);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View root = inflater.inflate(R.layout.fragment_map, container, false);
+        mMapView = root.findViewById(R.id.bmapView);
+        MapStatus.Builder builder = new MapStatus.Builder();
+        builder.zoom(18.0f);//精度设置
+        mBaiduMap = mMapView.getMap();
+        mBaiduMap.setMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+        mBaiduMap.setMyLocationEnabled(true);
+        //判断版本申请权限
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                root.getContext().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE);
+        }
+
+        //通过LocationClient发起定位
+        //定位初始化
+        MyLocationConfiguration mLocationConfiguration = null;
+        try {
+            mLocationClient = new LocationClient(getContext());
+            //通过LocationClientOption设置LocationClient相关参数
+            LocationClientOption option = new LocationClientOption();
+            option.setOpenGps(true); // 打开gps
+            option.setCoorType("bd09ll"); // 设置坐标类型
+            option.setScanSpan(1000);//设置定位时间
+            //设置locationClientOption
+            mLocationClient.setLocOption(option);
+            //注册LocationListener监听器
+            MyLocationListener myLocationListener = new MyLocationListener();
+            mLocationClient.registerLocationListener(myLocationListener);
+            //开启地图定位图层
+            mLocationClient.start();
+            mLocationConfiguration = new MyLocationConfiguration(
+                    MyLocationConfiguration.LocationMode.COMPASS, true, null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mBaiduMap.setMyLocationConfiguration(mLocationConfiguration);
+
+        //POI检索
+        mPoiSearch = PoiSearch.newInstance();
+        OnGetPoiSearchResultListener listener = new OnGetPoiSearchResultListener() {
+            @Override
+            public void onGetPoiResult(PoiResult poiResult) {
+                if (poiResult.error == SearchResult.ERRORNO.NO_ERROR) {
+                    mBaiduMap.clear();
+
+                    //创建PoiOverlay对象
+                    MyPoiOverlay poiOverlay = new MyPoiOverlay(mBaiduMap);
+
+                    //设置Poi检索数据
+                    poiOverlay.setData(poiResult);
+
+                    //将poiOverlay添加至地图并缩放至合适级别
+                    poiOverlay.addToMap();
+                    poiOverlay.zoomToSpan();
+                    mBaiduMap.setOnMarkerClickListener(poiOverlay);
+                }
+
+            }
+            @Override
+            public void onGetPoiDetailResult(PoiDetailSearchResult poiDetailSearchResult) {
+
+            }
+            @Override
+            public void onGetPoiIndoorResult(PoiIndoorResult poiIndoorResult) {
+
+            }
+            //废弃
+            @Override
+            public void onGetPoiDetailResult(PoiDetailResult poiDetailResult) {
+
+            }
+        };
+        //设置检索监听器
+        mPoiSearch.setOnGetPoiSearchResultListener(listener);
+        EditText editText=root.findViewById(R.id.editText);
+        Button button=root.findViewById(R.id.button);
+        button.setOnClickListener(view -> {
+            //String keyword = editText.getText().toString();
+            mPoiSearch.searchNearby(new PoiNearbySearchOption()
+                    .location(new LatLng(31.0634,120.4821))
+                    .radius(100000000)
+            //支持多个关键字并集检索，不同关键字间以$符号分隔，最多支持10个关键字检索。如:”银行$酒店”
+                    .keyword("suzhou"));
+        });
+
+
+        return root;
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mLocationClient.stop();
+        mBaiduMap.setMyLocationEnabled(false);
+        mMapView.onDestroy();
+        mMapView = null;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults[0]==PackageManager.PERMISSION_GRANTED){
+            mLocationClient.restart();
+        }
+        else {
+            Toast.makeText(getContext(),"你拒绝了GPS点位权限，无法定位",Toast.LENGTH_SHORT).show();
+        }
+    }
+}
